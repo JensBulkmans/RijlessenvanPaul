933d4bbb4cae51adbc3e158aff2f27a87becc89a
Jens-Bulkmans <117443870+Jens-Bulkmans@users.noreply.github.com>
2022-11-10 10:52:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,19 @@
 			<h1>Home</h1>
 
 		</div>
+			<h1>Javascript-button</h1>
+	<p id ="welkom"></p>
+	<button id="button">Toepasselijke text</button>
+
+	<script>
+		document.getElementById('button').addEventListener('click', function() {
+			let name = prompt("Wat is je naam?");
+			if (name.length > 0) {
+				document.getElementById('welkom').innerText = "Welkom op deze pagina, " + name;
+				document.getElementById('welkom').style.fontSize = '55px';
+				document.getElementById('welkom').style.backgroundColor = 'yellow';
+			}})
+</script>
 	</main>
 
 

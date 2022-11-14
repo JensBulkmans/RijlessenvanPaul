0edc953e94c93a694cca7a6b67a664345ac1077f
JensBulkmans <117359146+JensBulkmans@users.noreply.github.com>
2022-11-14 12:31:42 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <header>
     <div class="topnav" id="myTopnav">
+        <img src="img/LogoBlom.png" alt="Logo Blom">
         <div class="navimg">
-            <img src="img/LogoBlom.png" alt="Logo Blom">
-        </div>
         <a href="index.php" class="active">Home</a>
       	<a href="Lessen.php">Lessen</a>
         <a href="Prijzen.php">Prijzen</a>
         <a href="Over ons.php">Over ons</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        </a>
         <i class="fa fa-bars"></i>
         </a>
     </div>

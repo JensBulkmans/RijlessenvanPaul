f78441bbf836a70598d0a3337274b502c240f6fa
JensBulkmans <117359146+JensBulkmans@users.noreply.github.com>
2022-11-15 17:19:35 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,5 +3,5 @@
 		<p>&copy; 2022 Jens & Julian - studenten bij Curio.</p>
 		<p class="muted">Deze website is gebouwd als schoolopdracht voor het eerste leerjaar van de opleiding <em>Software Developer</em></p>
 					<a href="contact.php">Klik hier voor het contactformulier &gt;</a>
-</div>
 </footer>
+</div>
